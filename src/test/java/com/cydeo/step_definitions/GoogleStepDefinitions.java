8ddd7b4efdf8edd905203a72b1518f4bbb132a78
makifermis <makifermis@gmail.com>
2022-06-15 14:30:27 +0200

    day16

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,25 +1,61 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
+
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
+    @When("user types apple and clicks enter")
+    public void user_types_and_clicks_enter2() {
+        googleSearchPage.refuseCookiesButton.click();
+        googleSearchPage.searchBox.sendKeys("apple"+Keys.ENTER);
+    }
+
+    @When("user types {string} and clicks enter")
+    public void user_types_and_clicks_enter(String searchKeyword) {
+        googleSearchPage.refuseCookiesButton.click();
+        googleSearchPage.searchBox.sendKeys(searchKeyword+Keys.ENTER);
+    }
+
+    @Then("user sees {string} in the google title")
+    public void user_sees_in_the_google_title(String string) {
+        String expectedTitle = string+" - Recherche Google";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        //Junit assertion accepts first arg as expected, second arg as actual
+        Assert.assertEquals("Title is not as expected", expectedTitle, actualTitle);
+    }
+
+    @Then("user sees apple in the google title")
+    public void user_sees_apple_in_the_google_title() {
+        String expectedTitle = "apple - Recherche Google";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        //Junit assertion accepts first arg as expected, second arg as actual
+        Assert.assertEquals("Title is not as expected", expectedTitle, actualTitle);
+        // Assert.assertEquals(actualTitle.equals(expectedTitle));
+    }
 
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
 
         Driver.getDriver().get("https://www.google.com");
     }
+
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
         String expectedTitle = "Google";
         String actualTitle = Driver.getDriver().getTitle();
 
-        Assert.assertEquals(actualTitle,expectedTitle);
+        Assert.assertEquals(expectedTitle, actualTitle);
 
-        Driver.closeDriver();
+
     }
 
 }
